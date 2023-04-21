bb0f9b5958311b061f43797861a88aede8c37959
flawlezfox <armanarakelyan33@gmail.com>
2023-04-21 18:59:07 +0400

    F3 added basic layout

--- client/my-app/src/common/el/header.tsx
+++ client/my-app/src/common/el/header.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import "../styles/header.css"
+import { ReactComponent as CartSvg } from "../../assets/cart-icon.svg";
+import { ReactComponent as HeartFilledSvg } from "../../assets/heart-icon-filled.svg";
 
 const Header = () => {
     return(
-        <div>
-            This is a header;
+        <div className="Header">
+            <div className="logo-container">
+                <a href="/home">
+                    <img src={require("../../assets/logo.png")} alt="logo" />
+                </a>
+            </div>
+
+            <div className="pages-navigation">
+                <div className="home-link">
+                    <a href="/home">Главная</a>
+                </div>
+
+                <div className="catalogue-link">
+                    <a href="/catalogue">Каталог</a>
+                </div>
+            </div>
+
+            <div className="catalogue-navigation">
+                <input type="text" placeholder="Введите запрос"/>
+
+                <div className="heart-icon-container">
+                    <a href="/favorites">
+                        <HeartFilledSvg />
+                    </a>
+                </div>
+
+                <div className="cart-icon-container">
+                    <a href="/cart">
+                        <CartSvg />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
