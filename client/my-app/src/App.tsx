4b5c6cf92a86a5af60533a62a04a8859c7ba9cd4
DonskovDV <daniladonskov01@gmail.com>
2023-04-24 16:45:39 +0400

    F4 added routes

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Header from './common/el/header';
+import PageHome from './pages/homepage/view/page_home';
+import PageCatalogue from './pages/catalogue_page/view/page_catalogue';
+import PageFavorites from './pages/favorites_page/view/page_favorites';
+import PageCart from './pages/cart_page/view/page_cart';
+import { Route, Routes } from 'react-router-dom';
 import "./App.css"
+
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<PageHome />} />
+        <Route path="/catalogue" element={<PageCatalogue />} />
+        <Route path="/favorites" element={<PageFavorites />} />
+        <Route path="/cart" element={<PageCart />} />
+      </Routes>
+    </>
   );
 }
 
