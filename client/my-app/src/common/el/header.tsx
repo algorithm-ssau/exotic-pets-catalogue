a3a1304724ee3e48b6be09eca33875ff5ea59739
DonskovDV <daniladonskov01@gmail.com>
2023-04-24 16:59:11 +0400

    F4 added links for routes

--- client/my-app/src/common/el/header.tsx
+++ client/my-app/src/common/el/header.tsx
@@ -2,42 +2,43 @@ import React from 'react';
 import "../styles/header.css"
 import { ReactComponent as CartSvg } from "../../assets/cart-icon.svg";
 import { ReactComponent as HeartFilledSvg } from "../../assets/heart-icon-filled.svg";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return(
+    return (
         <div className="Header">
             <div className="header-wrapper">
                 <div className="logo-container">
-                    <a href="/home">
+                    <NavLink to="/">
                         <img src={require("../../assets/logo.png")} alt="logo" />
-                    </a>
+                    </NavLink>
                 </div>
 
                 <div className="pages-navigation">
                     <div className="home-link">
-                        <a href="/home">главная</a>
+                        <NavLink to="/">главная</NavLink>
                     </div>
 
                     <div className="catalogue-link">
-                        <a href="/catalogue">каталог</a>
+                        <NavLink to="/catalogue">каталог</NavLink>
                     </div>
                 </div>
 
                 <div className="catalogue-navigation">
                     <div className="search-container">
-                        <input type="text" placeholder="Введите запрос"/>
+                        <input type="text" placeholder="Введите запрос" />
                     </div>
 
                     <div className="heart-icon-container">
-                        <a href="/favorites">
+                        <NavLink to="/favorites">
                             <HeartFilledSvg />
-                        </a>
+                        </NavLink>
                     </div>
 
                     <div className="cart-icon-container">
-                        <a href="/cart">
+                        <NavLink to="/cart">
                             <CartSvg />
-                        </a>
+                        </NavLink>
                     </div>
                 </div>
             </div>
