8326132cabe445388afdc0931b8a391631660f8d
flawlezfox <armanarakelyan33@gmail.com>
2023-05-08 20:52:32 +0400

    F6 modified bullet for pagination

--- client/my-app/src/pages/homepage/el/Slider.tsx
+++ client/my-app/src/pages/homepage/el/Slider.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
 
-import "swiper/css"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Slider = () => {
     return (
         <div className="slider-container">
-            <Swiper>
+            <Swiper
+                modules={[Navigation, Pagination]}
+                slidesPerView={1}
+                navigation
+                pagination={{clickable: true}}
+            >
                 <SwiperSlide>
-                    Slide 1
+                    <img src={require("../../../temp/image 30.png")} alt="" width={1110}/>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    Slide 2
+                    <img src={require("../../../temp/image 31.png")} alt="" width={1110}/>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    Slide 3
+                    <img src={require("../../../temp/image 32.png")} alt="" width={1110}/>
                 </SwiperSlide>
             </Swiper>
         </div>
