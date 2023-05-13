7deda162e12f65716cc6fe7b117a6b1a5ada1a31
flawlezfox <armanarakelyan33@gmail.com>
2023-05-13 21:38:19 +0400

    F7 added temporary species data

---
+++ client/my-app/src/pages/homepage/el/SpeciesCard/data.ts
@@ -0,0 +1,40 @@
+interface ISpecies {
+    id?: number;
+    species: string;
+    image: string; // contains url to the image
+}
+
+const speciesData: ISpecies[] = [
+    {
+        id: 1,
+        species: "Рептилии",
+        image: "https://i.ibb.co/wKdkzmx/image-31.png",
+    },
+    {
+        id: 2,
+        species: "Птицы",
+        image: "https://i.ibb.co/b3sZM28/image-31-2.png",
+    },
+    {
+        id: 3,
+        species: "Кошки",
+        image: "https://i.ibb.co/vXvyGr6/image-31-3.png",
+    },
+    {
+        id: 4,
+        species: "Лисы",
+        image: "https://i.ibb.co/WPXdZQW/image-31-4.png",
+    },
+    {
+        id: 5,
+        species: "Насекомые",
+        image: "https://i.ibb.co/kcqvSYR/image-31-5.png",
+    },
+    {
+        id: 6,
+        species: "Пауки",
+        image: "https://i.ibb.co/Lp9mWZH/image-31-6.png",
+    },
+]
+
+export default speciesData;
