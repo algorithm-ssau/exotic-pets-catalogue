bf57b11e790b6b1e30dee87f1c10536ec23b104c
flawlezfox <armanarakelyan33@gmail.com>
2023-05-14 16:27:20 +0400

    F7 added remaining layout

--- client/my-app/src/pages/homepage/view/page_home.tsx
+++ client/my-app/src/pages/homepage/view/page_home.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+
+// components
 import Slider from '../el/Slider';
+import Button from '../el/Button';
+import SpeciesCard from '../el/SpeciesCard';
+
+// data / interfaces / temporary files
+import speciesData from '../el/SpeciesCard/data';
+
+//styles
 import "../styles/page_home.css"
 import "../styles/swiper_custom.css"
 
@@ -12,6 +21,35 @@ const PageHome = () => {
                 <span className="company-intro">
                     Exotic Pets - крупнейший магазин экзотических питомцев
                 </span>
+            </div>
+
+            <div className="species-info-container">
+                <div className="species-info-header">
+                    <h2>
+                        <span className="text-accent">Огромный</span> выбор видов животных
+                    </h2>
+
+                    <p className="find-your-specie">
+                        Найдите питомца по душе
+                    </p>
+
+                    <Button 
+                        className={"catalogue-button"}
+                        isLink={true}
+                        act={"Каталог"}
+                    />
+                </div>
+                
+                {speciesData.map(specie => {
+                    return (
+                        <li key={specie.id}>
+                            <SpeciesCard
+                                species={specie.species}
+                                image={specie.image}
+                            />
+                        </li>
+                    );
+                })}
             </div>
         </div>
     );
