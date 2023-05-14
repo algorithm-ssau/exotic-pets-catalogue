3691cae809275ce2d9750fca89c6da8eda080161
GriG <oleggrirorev@yandex.ru>
2023-05-14 23:44:21 +0400

    B4 fixed B3 knex connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
 
-
-let knex;
-
-fetch('https://127.0.0.1:3000/config.json')
-    .then((response) => response.json())
-    .then((json) => {
-        let config = JSON.parse(json);
-        knex = require('knex')(config);
-    });
+const config = require('.\\config.json');
+const knex = require('knex')(config);
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
