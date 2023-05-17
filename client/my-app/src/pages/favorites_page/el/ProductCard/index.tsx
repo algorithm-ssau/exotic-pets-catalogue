d0cc0199c26fe496284f057b566347aad110e297
flawlezfox <armanarakelyan33@gmail.com>
2023-05-17 19:13:42 +0400

    F14 product card initialization

---
+++ client/my-app/src/pages/favorites_page/el/ProductCard/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import "./index.css";
+
+const ProductCard = () => {
+    return (
+        <div className="product-card-container">
+
+        </div>
+    );
+}
+
+export default ProductCard;
