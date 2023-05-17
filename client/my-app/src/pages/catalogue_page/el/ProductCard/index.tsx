0b349e0adbde6b5a3f4660bc4da080365687f695
flawlezfox <armanarakelyan33@gmail.com>
2023-05-17 20:58:28 +0400

    F14 added changing favorite state

--- client/my-app/src/pages/catalogue_page/el/ProductCard/index.tsx
+++ client/my-app/src/pages/catalogue_page/el/ProductCard/index.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { ReactComponent as HeartSvg } from "../../../../assets/heart-icon.svg";
+import { ReactComponent as HeartFilledSvg } from "../../../../assets/heart-icon-filled.svg";
 import { ReactComponent as CartSvg } from "../../../../assets/cart-icon.svg";
+import IProduct from '../../../../common/interfaces/IProduct';
 
 import "./index.css";
 
-const ProductCard = () => {
+const ProductCard = ({id, name, price, isFavourite = false, isInCart = false}: IProduct) => {
+    const [isFav, setIsFav] = useState<boolean>(isFavourite);
+
+    function handleSetIsFav() {
+        if (isFav === false) {
+            setIsFav(true);
+        } else {
+            setIsFav(false);
+        }
+    }
+
     return (
         <div className="product-card-container" style={{backgroundImage: `url(${"https://i.pinimg.com/originals/23/d3/63/23d363aa71b1e9b8498fa49b2f357e6d.jpg"}`}}>
-            <div className="product-card-favourite">
-                <HeartSvg />
+            <div 
+                className="product-card-favourite"
+                onClick={handleSetIsFav}
+            >
+                {isFav ? <HeartFilledSvg /> : <HeartSvg />}
             </div>
 
             <div className="product-card-info">
