0bc9ba26b747a0e40c74813ae35235634fb57c9b
flawlezfox <armanarakelyan33@gmail.com>
2023-05-17 20:46:58 +0400

    F14 added basic layout

--- client/my-app/src/pages/catalogue_page/el/ProductCard/index.tsx
+++ client/my-app/src/pages/catalogue_page/el/ProductCard/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
+import { ReactComponent as HeartSvg } from "../../../../assets/heart-icon.svg";
+import { ReactComponent as CartSvg } from "../../../../assets/cart-icon.svg";
 
 import "./index.css";
 
 const ProductCard = () => {
     return (
-        <div className="product-card-container">
+        <div className="product-card-container" style={{backgroundImage: `url(${"https://i.pinimg.com/originals/23/d3/63/23d363aa71b1e9b8498fa49b2f357e6d.jpg"}`}}>
+            <div className="product-card-favourite">
+                <HeartSvg />
+            </div>
 
+            <div className="product-card-info">
+                <div className="product-card-name">
+                    Эублефар крутейший
+                </div>
+
+                <div className="product-card-info-left">
+                    <div className="product-card-price">
+                        250000 ₽
+                    </div>
+
+                    <button className="product-card-button">
+                        <CartSvg 
+                            width={15} 
+                            height={15}
+                        />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
