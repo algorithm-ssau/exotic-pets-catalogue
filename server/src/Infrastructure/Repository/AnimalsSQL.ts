dc131794deb48fb257e53cf66da5d40fb73db6dc
GriG <oleggrirorev@yandex.ru>
2023-05-18 19:12:44 +0400

    added kingdom filter sql query

--- server/src/Infrastructure/Repository/AnimalsSQL.ts
+++ server/src/Infrastructure/Repository/AnimalsSQL.ts
@@ -1,4 +1,4 @@
-import { AnimalsE, AnimalsWithImageI, ImageE } from "../Entity/AnimalsE";
+import { AnimalsE, AnimalsWithImageI, ImageE, KingdomsE, SpeciesE, catalogueCardI} from "../Entity/AnimalsE";
 import config from "../../../config";
 import knex, { Knex } from "knex";
 
@@ -20,6 +20,24 @@ export class AnimalsSQL {
                 .first();
         } catch (e) {
             console.log('getAnimalById sql ERROR', e);
+        }
+
+        return vAnimal;
+    }
+
+    public async getAnimalsOfSpecifiedKingdom(sKingdom: string): Promise<catalogueCardI> {
+        let vAnimal: catalogueCardI = {};
+
+        try {
+            vAnimal = await this.db<catalogueCardI>({king: KingdomsE.NAME})
+            .leftJoin({spc: SpeciesE.NAME}, 'spc.kingdoms_id', 'king.id')
+            .leftJoin({anima: AnimalsE.NAME}, 'anima.species_id', 'spc.id')
+            .leftJoin({img: ImageE.NAME}, 'img.id', 'anima.images_id')
+            .where('king.name', sKingdom)
+            .select('anima.id', 'anima.name', 'anima.price','img.image');
+
+        } catch (e) {
+            console.log('getAnimalsOfSpecifiedKingdom sql ERROR', e);
         }
 
         return vAnimal;
