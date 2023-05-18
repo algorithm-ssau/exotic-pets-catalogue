7d4fbae486af418d16e0dd43a2cb9be06f2998e3
GriG <oleggrirorev@yandex.ru>
2023-05-18 22:34:51 +0400

    spawned comment entities

---
+++ server/src/Infrastructure/Entity/CommentsE.ts
@@ -0,0 +1,12 @@
+export interface ImageI {
+    id?: number;
+    image?: string;
+}
+
+export class ImageE {
+    public static NAME = 'images'
+}
+
+export class CommentsTableE {
+    public static NAME = 'comments'
+}
