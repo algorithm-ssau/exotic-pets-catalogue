76859c17be33205fbc58469ff1337a5654f6b940
DyeMeat <125212749+DyeMeat@users.noreply.github.com>
2023-05-18 20:20:49 +0400

    F15 improved filter

--- client/my-app/src/pages/catalogue_page/view/el/Filter/index.tsx
+++ client/my-app/src/pages/catalogue_page/view/el/Filter/index.tsx
@@ -2,45 +2,16 @@ import * as React from 'react';
 
 import './index.css';
 
+import Filter from './Filter';
+
 const CatalogueFilter = () => {
     return (
         <div className='catalogue-filter'>
-                <select className='filter-list' name='kingdom'>
-                    <option disabled selected>Класс животного</option>
-                    <option>Все</option>
-                    <option value ='1'>Млекопитающие</option>
-                    <option value ='2'>Птицы</option>
-                    <option value ='3'>Рептилии</option>
-                    <option value ='4'>Земноводные</option>
-                    <option value ='5'>Рыбы</option>
-                    <option value ='6'>Насекомые</option>
-                    <option value ='7'>Паукообразные</option>
-                </select>
 
-                
-                <select className='filter-list' name='species'>
-                    <option disabled selected>Вид животного</option>
-                    <option>Все</option>
-                    <option value ='1'>Приматы</option>
-                    <option value ='2'>Лисицы</option>
-                    <option value ='3'>Попугаи</option>
-                    <option value ='4'>Совы</option>
-                    <option value ='5'>Лягушки</option>
-                    <option value ='6'>Ящерицы</option>
-                    <option value ='7'>Крокодилы</option>
-                    <option value ='8'>Черепахи</option>
-                    <option value ='9'>Кошки</option>
-                    <option value ='10'>Скорпионы</option>
-                    <option value ='11'>Пауки</option>
-                    <option value ='12'>Муравьи</option>
-                    <option value ='13'>Жуки</option>
-                    <option value ='14'>Хрящевые рыбы</option>
-                    <option value ='15'>Костные рыбы</option>
-                    <option value ='16'>Жабы</option>
-                </select>    
-
+                <Filter></Filter>
 
                 <input className='search-bar' type="text" placeholder="Поиск по каталогу" />
+                
         </div>
     );
 }
