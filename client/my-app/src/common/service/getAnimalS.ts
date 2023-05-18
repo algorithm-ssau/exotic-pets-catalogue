064102218246f61b12ea22ea4f073d5f29463c7f
flawlezfox <armanarakelyan33@gmail.com>
2023-05-18 18:39:44 +0400

    F16 added service to get animals data from server

---
+++ client/my-app/src/common/service/getAnimalS.ts
@@ -0,0 +1,35 @@
+import IProduct from "../interfaces/IProduct";
+
+export async function getSpeciesAnimals(species: string): Promise<IProduct[]> {
+    const response = await fetch('http://25.13.222.253:5000/animals/get-species', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            species: species
+        })
+    })
+
+    return response.json();
+}
+
+export async function getKingdomAnimals(kingdom: string): Promise<IProduct[]> {
+    const response = await fetch('http://25.13.222.253:5000/animals/get-kingdom', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            kingdom: kingdom
+        })
+    })
+
+    return response.json();
+}
+
+export async function getAllAnimals(): Promise<IProduct[]> {
+    const response = await fetch('http://25.13.222.253:5000/animals/get-all');
+
+    return response.json();
+}
