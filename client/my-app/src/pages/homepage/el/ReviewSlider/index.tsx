258cde162ab00fa1eefea6e873dd0e5067e2902a
flawlezfox <armanarakelyan33@gmail.com>
2023-05-19 15:14:07 +0400

    F12 reviews now can display on the home page

--- client/my-app/src/pages/homepage/el/ReviewSlider/index.tsx
+++ client/my-app/src/pages/homepage/el/ReviewSlider/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation } from 'swiper';
 
 import ReviewCard from '../ReviewCard';
+import { getAllReviews } from '../../../../common/service/reviewS';
+import IReview from "../../../../common/interfaces/IReview"
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+async function getReview(f: () => Promise<IReview[]>): Promise<IReview[]> {
+    const reviews = await f();
+
+    return reviews;
+}
+
 const ReviewSlider = () => {
+    const [reviews, setReviews] = useState<IReview[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getReview(getAllReviews);
+            setReviews(data);
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <div className="review-container slider-container">
             <Swiper
@@ -15,37 +34,18 @@ const ReviewSlider = () => {
                 slidesPerView={3}
                 navigation
             >
-                <SwiperSlide>
-                    <ReviewCard
-                        image={""}
-                        name="Легушка"
-                        review="Отличный сервис. Крутой очень. Лорем ипсум даже я бы сказал"
-                    />
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <ReviewCard
-                            image={""}
-                            name="Легушка"
-                            review="Отличный сервис. Крутой очень. Лорем ипсум даже я бы сказал"
-                    />
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <ReviewCard
-                        image={""}
-                        name="Легушка"
-                        review="Отличный сервис. Крутой очень. Лорем ипсум даже я бы сказал"
-                    />
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <ReviewCard
-                        image={""}
-                        name="Легушка"
-                        review="Отличный сервис. Крутой очень. Лорем ипсум даже я бы сказал"
-                    />
-                </SwiperSlide>
+                {reviews.reverse().map(review => {
+                    console.log(review.user_name)
+                    return (
+                        <SwiperSlide>
+                            <ReviewCard
+                                image={review.image}
+                                user_name={review.user_name}
+                                com_body={review.com_body}
+                            />
+                        </SwiperSlide>
+                    )
+                })}
             </Swiper>
         </div>
     );
