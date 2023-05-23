5688312d5ac9b6704b772b862417b9843145069e
flawlezfox <armanarakelyan33@gmail.com>
2023-05-23 18:08:21 +0400

    F17 added enums for catalogue page

---
+++ client/my-app/src/pages/catalogue_page/view/ifc_catalogue.ts
@@ -0,0 +1,30 @@
+export enum kingdomFilterT {
+    all = 0,
+    mammals = 1,
+    birds = 2,
+    reptiles = 3,
+    frogs = 4,
+    fish = 5,
+    insects = 6,
+    spiders = 7
+}
+
+export enum speciesFilterT {
+    all = 0,
+    primates = 1,
+    foxes = 2,
+    parrots = 3,
+    owls = 4,
+    frogs = 5,
+    lizards = 6,
+    crocs = 7,
+    turtle = 8,
+    cats = 9,
+    scorpions = 10,
+    spiders = 11,
+    ants = 12, 
+    bugs = 13,
+    fish1 = 14,
+    fish2 = 15,
+    bigFrogs = 16
+}
