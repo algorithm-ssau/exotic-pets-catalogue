08b8609af475f07d1979b37a85ca4d3106c44ed0
DonskovDV <daniladonskov01@gmail.com>
2023-05-24 18:39:26 +0300

    F19 added cart page style

--- client/my-app/src/pages/cart_page/view/page_cart.tsx
+++ client/my-app/src/pages/cart_page/view/page_cart.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import Payment from '../el/Payment';
+
+import "../styles/cart_page.css"
+
 const PageCart = () => {
     return (
-        <h1>Cart</h1>
+        // <h1>Cart</h1>
+        
+        <div className="cartpage-container">
+             <div className="payment-container">
+                 <Payment
+                 />
+        </div>
+        </div>
+       
     );
 }
 
