89c2c4c59460e99501c7aaef25a3b7b338e824cd
flawlezfox <armanarakelyan33@gmail.com>
2023-05-24 18:23:10 +0400

    F17 now the products is shown as a grid

--- client/my-app/src/pages/catalogue_page/el/Products/index.tsx
+++ client/my-app/src/pages/catalogue_page/el/Products/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { observer } from "mobx-react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
 
 // interfaces, components
 import IProduct from '../../../../common/interfaces/IProduct';
@@ -14,17 +12,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import "./index.css";
 
-// services 
-import { getSpeciesAnimals, getKingdomAnimals, getAllAnimals } from '../../../../common/service/getAnimalS';
-
-// function to get animals data, that takes function that decides which group of animals would be taken
-async function getAnimals(f: (param?: string) => Promise<IProduct[]>, param: string = ""): Promise<IProduct[]> {
-    const animals = await f(param);
-
-    return animals;
-}
-
-const ProductSlider = () => {
+const Products = () => {
     const [, setAnimals] = useState<IProduct[]>([]);
 
     useEffect(() => {
@@ -37,28 +25,21 @@ const ProductSlider = () => {
     }, []);
 
     return (
-        <div className="catalogue-slider-container">
-            <Swiper
-                modules={[Navigation]}
-                spaceBetween={30}
-                slidesPerView={3}
-                navigation
-            >
-                {productStore.products.map(animal => {
+        <div className="catalogue-products-container">
+            {productStore.products.map(animal => {
                     return (
-                        <SwiperSlide key={animal.id}>
+                        <li key={animal.id}>
                             <ProductCard
                                 id={animal.id}
                                 image={animal.image}
                                 name={animal.name}
                                 price={animal.price}
                             />
-                        </SwiperSlide>
+                        </li>
                     )
                 })}
-            </Swiper>
         </div>
     );
 }
 
-export default observer(ProductSlider);
+export default observer(Products);
--- client/my-app/src/pages/catalogue_page/view/page_catalogue.tsx
+++ client/my-app/src/pages/catalogue_page/view/page_catalogue.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { observer } from "mobx-react";
 
 import "../styles/page_catalogue.css";
@@ -6,14 +6,12 @@ import "../../homepage/styles/swiper_custom.css"
 
 
 // components
-import ProductCard from '../el/ProductCard';
-import ProductSlider from '../el/ProductSlider';
-import Filter from '../el/Filter';
+import Products from '../el/Products';
 import productStore from '../../../store/productStore';
+import CatalogueFilter from '../el/Filter';
 
 //styles
 import "../styles/page_catalogue.css";
-import CatalogueFilter from '../el/Filter';
 
 const PageCatalogue = () => {
 
@@ -24,16 +22,12 @@ const PageCatalogue = () => {
             </div>
 
             {
-                productStore.selectedKingdom === "all" ?
-                <div className="popular-products-container">
-                    <h2 className="products-header">Популярное</h2>
-                </div> :
-                <div className="popular-products-container">
-                    <h2 className="products-header">{productStore.getKingdomLabel(productStore.selectedKingdom)}</h2>
-                </div>
+                productStore.selectedKingdom === 0
+                    ? <h2 className="products-header">Весь каталог</h2>
+                    : <h2 className="products-header">{productStore.selectedKingdom}</h2>
             }
             
-            <ProductSlider />
+            <Products />
         </div>
     );
 }
