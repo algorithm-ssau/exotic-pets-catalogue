cec3c7d63d695d998f34bd7a1247553fdb980a9c
DyeMeat <125212749+DyeMeat@users.noreply.github.com>
2023-05-24 23:49:52 +0400

    F18 product card added to cart page

--- client/my-app/src/pages/cart_page/view/page_cart.tsx
+++ client/my-app/src/pages/cart_page/view/page_cart.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import CartProductCard from '../el/CartProductCard';
+
+//styles
+import "../styles/page_cart.css";
+
+let id = 1;
+let image = "../../../../assets/image_30.png";
+let name = 'Электричекский Синий День Геккон';
+let price=  50000;
+let description = 'Таинственный обитатель заснеженных альпийских вершин';
+
+
 const PageCart = () => {
     return (
-        <h1>Cart</h1>
+            <div className='cart-product-card-container'>
+                <CartProductCard id = {id} image = {image} name = {name} price = {price} description = {description}></CartProductCard>
+            </div>
     );
 }
 
