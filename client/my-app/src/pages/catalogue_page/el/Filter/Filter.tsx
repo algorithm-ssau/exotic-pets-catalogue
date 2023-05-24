8868a6a25971b3dab069be9ecec4dfe990161bc7
flawlezfox <armanarakelyan33@gmail.com>
2023-05-24 22:10:21 +0400

    F17 added filtering depending on kingdom or specie

--- client/my-app/src/pages/catalogue_page/el/Filter/Filter.tsx
+++ client/my-app/src/pages/catalogue_page/el/Filter/Filter.tsx
@@ -14,112 +14,10 @@ const Filter = () => {
     {label: "Паукообразные", value: kingdomFilterT.spiders},
   ]
   
-  const speciesOptions = [
-    { label: "Все", value: speciesFilterT.all },
-    { label: "Приматы", value: speciesFilterT.primates },
-    { label: "Лисицы", value: speciesFilterT.foxes },
-    { label: "Попугаи", value: speciesFilterT.parrots },
-    { label: "Совы", value: speciesFilterT.owls },
-    { label: "Лягушки", value: speciesFilterT.frogs },
-    { label: "Ящерицы", value: speciesFilterT.lizards },
-    { label: "Крокодилы", value: speciesFilterT.crocs },
-    { label: "Черепахи", value: speciesFilterT.turtle },
-    { label: "Кошки", value: speciesFilterT.cats },
-    { label: "Скорпионы", value: speciesFilterT.scorpions },
-    { label: "Пауки", value: speciesFilterT.spiders },
-    { label: "Муравьи", value: speciesFilterT.ants },
-    { label: "Жуки", value: speciesFilterT.bugs },
-    { label: "Хрящевые рыбы", value: speciesFilterT.fish1 },
-    { label: "Костные рыбы", value: speciesFilterT.fish2 },
-    { label: "Жабы", value: speciesFilterT.bigFrogs }
-  ];
-
   const handleKingdomChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue: number = +event.target.value;
 
     productStore.setKingdom(selectedValue);
-    
-    switch (selectedValue) {
-      case kingdomFilterT.mammals:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Приматы", value: "1" },
-          { label: "Лисицы", value: "2" },
-          { label: "Кошки", value: "9" }
-        ]);
-        break;
-
-      case kingdomFilterT.birds:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Попугаи", value: "3" },
-          { label: "Совы", value: "4" }
-        ]);
-        break;
-
-      case kingdomFilterT.reptiles:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Ящерицы", value: "6" },
-          { label: "Крокодилы", value: "7" },
-          { label: "Черепахи", value: "8" }
-        ]);
-        break;
-
-      case kingdomFilterT.frogs:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Лягушки", value: "5" },
-          { label: "Жабы", value: "16" }
-        ]);
-        break;
-
-      case kingdomFilterT.fish:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Хрящевые рыбы", value: "14" },
-          { label: "Костные рыбы", value: "15" }
-        ]);
-        break;
-
-      case kingdomFilterT.insects:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Муравьи", value: "12" },
-          { label: "Жуки", value: "13" }
-        ]);
-        break;
-
-      case kingdomFilterT.spiders:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Скорпионы", value: "10" },
-          { label: "Пауки", value: "11" }
-        ]);
-        break;
-
-      default:
-        productStore.setSpeciesOptions([
-          { label: "Все", value: "all" },
-          { label: "Приматы", value: "1" },
-          { label: "Лисицы", value: "2" },
-          { label: "Попугаи", value: "3" },
-          { label: "Совы", value: "4" },
-          { label: "Лягушки", value: "5" },
-          { label: "Ящерицы", value: "6" },
-          { label: "Крокодилы", value: "7" },
-          { label: "Черепахи", value: "8" },
-          { label: "Кошки", value: "9" },
-          { label: "Скорпионы", value: "10" },
-          { label: "Пауки", value: "11" },
-          { label: "Муравьи", value: "12" },
-          { label: "Жуки", value: "13" },
-          { label: "Хрящевые рыбы", value: "14" },
-          { label: "Костные рыбы", value: "15" },
-          { label: "Жабы", value: "16" }
-        ]);
-        break;
-    }
   };
 
   const handleSpeciesChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -152,10 +50,10 @@ const Filter = () => {
         value={productStore.selectedSpecies}
         onChange={(e) => { handleSpeciesChange(e) }}
       >
-        {speciesOptions
+        {productStore.speciesOptions
           .map((species) => (
-            <option key={species.value} value={species.value}>
-              {species.label}
+            <option key={species.id} value={species.id}>
+              {species.name}
             </option>
           ))}
       </select>
--- client/my-app/src/pages/catalogue_page/view/page_catalogue.tsx
+++ client/my-app/src/pages/catalogue_page/view/page_catalogue.tsx
@@ -12,6 +12,18 @@ import CatalogueFilter from '../el/Filter';
 
 //styles
 import "../styles/page_catalogue.css";
+import { kingdomFilterT } from './ifc_catalogue';
+
+const kingdomOptions = [
+    {label: "Все", value: kingdomFilterT.all},
+    {label: "Млекопитающие", value: kingdomFilterT.mammals},
+    {label: "Птицы", value: kingdomFilterT.birds},
+    {label: "Рептилии", value: kingdomFilterT.reptiles},
+    {label: "Земноводные", value: kingdomFilterT.frogs},
+    {label: "Рыбы", value: kingdomFilterT.fish},
+    {label: "Насекомые", value: kingdomFilterT.insects},
+    {label: "Паукообразные", value: kingdomFilterT.spiders},
+  ]
 
 const PageCatalogue = () => {
 
@@ -24,7 +36,7 @@ const PageCatalogue = () => {
             {
                 productStore.selectedKingdom === 0
                     ? <h2 className="products-header">Весь каталог</h2>
-                    : <h2 className="products-header">{productStore.selectedKingdom}</h2>
+                    : <h2 className="products-header">{kingdomOptions.find(option => productStore.selectedKingdom === option.value)?.label}</h2>
             }
             
             <Products />
