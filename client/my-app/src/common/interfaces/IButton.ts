ba10ccaf69c1c3dc4c6ee356a672481d3d7950f5
flawlezfox <armanarakelyan33@gmail.com>
2023-05-26 23:05:29 +0400

    F19 updated button interface

--- client/my-app/src/common/interfaces/IButton.ts
+++ client/my-app/src/common/interfaces/IButton.ts
@@ -2,6 +2,7 @@ export default interface IButton {
     className: string;
     isLink?: boolean;
     act: string;
+    type?: "button" | "submit" | "reset" | undefined;
     onClick?: () => void;
     isDisabled?: boolean;
 }
