2bd44732933ca01273b7b7a65778aa3ae774948e
flawlezfox <armanarakelyan33@gmail.com>
2023-05-26 22:45:09 +0400

    F18 fixed the product card layout and styles

--- client/my-app/src/pages/cart_page/el/CartProductCard/index.tsx
+++ client/my-app/src/pages/cart_page/el/CartProductCard/index.tsx
@@ -4,42 +4,41 @@ import ICart from '../../../../common/interfaces/ICart';
 
 import "./index.css";
 
-const CartProductCard = ({id, image, name, price, description}: ICart) => {
+const CartProductCard = ({ id, image, name, price, description }: ICart) => {
     return (
         <div className='cart-product-card-container'>
-            
-            <img className="cart-product-card-image" src={require("../../../../assets/image_30.png")} alt='product_image'></img>
-            <div className='cart-product-card-info'>
-
-
-                <div className='cart-product-card-info-left'>
-                    <div className="cart-product-name">
-                        {name}
-                    </div>
-
-
-                    <div className="cart-product-description">
-                        {description}
-                    </div>
+            <div className='cart-product-card'>
+                
+                <div className="cart-product-card-image">
+                    <img src={require("../../../../assets/image_30.png")} alt='product_image' />
                 </div>
 
-                <div className='cart-product-card-info-right'>
-
-                    <button 
-                            className="cart-product-card-remove-button"
-                        >
-                            <CrossSvg 
-                                width={30} 
+                <div className="cart-product-card-info">
+                    <div className='cart-product-card-info-left'>
+                        <div className="cart-product-name">
+                            {name}
+                        </div>
+        
+        
+                        <div className="cart-product-description">
+                            {description}
+                        </div>
+                    </div>
+        
+                    <div className='cart-product-card-info-right'>
+                        <button className="cart-product-card-remove-button">
+                            <CrossSvg
+                                className="cross-icon"
+                                width={30}
                                 height={30}
                             />
-                    </button>
-
-                    <div className="cart-product-price">
-                        {price} ₽
+                        </button>
+        
+                        <div className="cart-product-price">
+                            {price} ₽
+                        </div>
                     </div>
-
                 </div>
-
             </div>
         </div>
     );
